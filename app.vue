015e71a239893f49fc73e2165b66210cae9285ea
Sieg Friedrich <siegfriedrich@outlook.com>
2023-06-19 22:02:12 +0900

    add auth page and menuoverlay

--- app.vue
+++ app.vue
@@ -1,4 +1,42 @@
 <template>
   <div class="fixed z-[-1] bg-[#f2f2f2] w-full h-[100vh]" />
   <NuxtPage />
+  <MenuOverlay
+    :class="[
+      {
+        'max-h-[100vh] transition-all duration-200 ease-in visible':
+          userStore.isMenuOverlay,
+      },
+      {
+        'max-h-0 transition-all duration-200 ease-out visible':
+          !userStore.isMenuOverlay,
+      },
+    ]"
+  />
 </template>
+<script setup>
+import { useUserStore } from "@/stores/user";
+const userStore = useUserStore();
+let windowWidth = ref(process.client ? window.innerWidth : "");
+const route = useRoute();
+userStore.isLoading = false;
+onMounted(() => {
+  window.addEventListener("resize", function () {
+    windowWidth.value = window.innerWidth;
+  });
+});
+
+watch(
+  () => windowWidth.value,
+  () => {
+    if (windowWidth.value >= 767) userStore.isMenuOverlay = false;
+  }
+);
+
+watch(
+  () => route.fullPath,
+  () => {
+    userStore.isLoading = true;
+  }
+);
+</script>
---
+++ pages/auth.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="AuthPage" class="w-full h-[100vh] bg-white">
+    <div
+      class="w-full flex items-center justify-center p-5 border-b border-b-gray-300"
+    >
+      <NuxtLink to="/" class="min-w-[170px]">
+        <img width="170" src="/AliExpress-logo.png" />
+      </NuxtLink>
+    </div>
+    <div class="max-w-[400px] mx-auto px-2">
+      <div class="text-center my-6">登録・ログイン</div>
+      <button
+        @click="login('google')"
+        class="flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+      >
+        <img class="w-full max-w-[30px]" src="/google-logo.png" />
+        <div>Google</div>
+      </button>
+      <button
+        @click="login('github')"
+        class="mt-4 flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+      >
+        <img class="w-full max-w-[30px]" src="/github-logo.png" />
+        <div>Github</div>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { client } from "process";
+const login = () => {
+  1 === 1;
+};
+
+// const client = useSupabaseClient();
+// const user = useSupabaseUser();
+// watchEffect(() => {
+//   if (user.value) {
+//     return navigateTo("/");
+//   }
+// });
+
+// const login = async (prov: string) => {
+//   const { data, error } = await client.auth.signInWithOAuth({
+//     provider: prov,
+//   });
+// };
+</script>
